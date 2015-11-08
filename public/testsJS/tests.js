9a9e6c1c3750a20058b01dfe2ea43b8bd8cc8601
oyama <oyama@projectcodex.co>
2015-11-08 22:00:38 +0000

    renamed Car class function and its object

--- public/testsJS/tests.js
+++ public/testsJS/tests.js
@@ -1,24 +1,24 @@
-QUnit.test("testing class function Cars", function(assert) {
-    assert.equal(typeof Cars, "function");
-    assert.equal(typeof(BMW.passenger_No), "number");
+QUnit.test("testing class function Car", function(assert) {
+    assert.equal(typeof Car, "function");
+    assert.equal(typeof(BMW.people), "number");
     assert.equal(typeof(BMW.color), "string");
     assert.equal(BMW.color, "Green");
 });
 
 QUnit.test("testing class Car properties", function(assert) {  
-	assert.equal(BMW.passenger_No, 5);
+	assert.equal(BMW.people, 5);
     assert.equal(BMW.color, "Green");
 
-    assert.equal(Mazda.passenger_No, 8);
+    assert.equal(Mazda.people, 8);
     assert.equal(Mazda.color, "White");
 
-    assert.equal(VW.passenger_No, 4);
+    assert.equal(VW.people, 4);
     assert.equal(VW.color, "Blue");
 
-    assert.equal(Ford.passenger_No, 6);
+    assert.equal(Ford.people, 6);
     assert.equal(Ford.color, "Red");
 
-    assert.equal(Nissan.passenger_No, 10);
+    assert.equal(Nissan.people, 10);
     assert.equal(Nissan.color, "Green");
 
 });
